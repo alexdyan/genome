d8ed79c9e9a5ea28072202f0a78bd6935b30c67d
Alexandra D <ady239@nyu.edu>
2021-07-18 22:18:49 -0400

    it moves using perlin noise

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ let coordsLen = 16;
 
 function setup() {
   createCanvas(600, 600);
-  background(255);
   noFill();
   stroke(255, 43, 43);
   strokeWeight(2);
+
+  noiseDetail(24);
   
   // fill the coordinates array with random data
   for (let i = 0; i < coordsLen; i++) {
@@ -15,16 +16,20 @@ function setup() {
 }
 
 function draw() {
+    background(245, 240, 240);
     // cycle through the coordinates to build the curves
     beginShape();
     curveVertex(coords[0].x, coords[0].y);
 
     for (let i = 0; i < coords.length - 1; i++) {
+        coords[i].float();
         curveVertex(coords[i].x, coords[i].y);
         // draw a circle on each data point
         strokeWeight(1)
         circle(coords[i].x, coords[i].y, 20);
         strokeWeight(2);
+        counterX += random(0.001, 0.01);
+        counterY += random(0.001, 0.01);
     }
     curveVertex(coords[coordsLen-1].x, coords[coordsLen-1].y)
     endShape();
@@ -35,16 +40,33 @@ class Coordinate {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.counter = 0;
+    this.interval = 0.01;
+    this.noiseOffsetX = random(1000);
+    this.noiseOffsetY = random(1000);
   }
   
-  // map each coordinate (pair?) to a sin wave to oscillate the wave and make it flowy
-//   oscillate() {
-//     // amplitude * sin(counter) + offset
-//     this.newX = 25 * sin(this.counter) + this.x;
-//     this.newY = 25 * sin(this.counter) + this.y;
-//     this.counter += 0.01;
-//   }
+  float() {
+      let moveX = map( noise(this.noiseOffsetX), 0, 1, -1, 1 );
+      let moveY = map( noise(this.noiseOffsetY), 0, 1, -1, 1 );
+      this.x += moveX;
+      this.y += moveY;
+      this.noiseOffsetX += this.interval;
+      this.noiseOffsetY += this.interval;
+
+      // bounce off the edges
+      if (this.x < 0) {
+        this.moveX = 100;
+      }
+      if (this.x > width) {
+        this.moveX = -100;
+      }
+      if (this.y < 0) {
+          this.moveY = 100;
+      }
+      if (this.y > height) {
+        this.moveY = -100;
+      }
+  }
 }
 
 
